c1506be6c68900d60aa311399e1681af08ff46a1
Priscila Ponce <priscilabponce@gmail.com>
2022-11-08 23:11:10 -0300

    PRODUCTOS

--- indexJS.js
+++ indexJS.js
@@ -1,31 +1,16 @@
 function inicio (){
-    let usuario = prompt(`¡Bienvenido a Fesno Negro! Ingrese su Nombre`)
-    let edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`)
-        while (edad <18){
-            alert(`Lo sentimos mucho ${usuario}, debes ser mayor de edad para poder ingresar.`)
-    let usuario = prompt(`¡Bienvenido a Fesno Negro! Ingrese su Nombre`)
-    let edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`)
-        }
-    let bienvenida = prompt(`¿Es tu primera vez en Fresno Negro? Ingrese Si o No`)
-    let rta
-    if (bienvenida === "Si"){
-        alert(`Bienvenido/a ${usuario}, es un placer tenerte con nosotros! Obtendras un descuento en tu primera compra`)
+    let usuario = prompt(`¡Bienvenido a Fesno Negro! Ingrese su Nombre`);
+    let edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`);
+    while (edad < 18){
+        alert(`Lo sentimos mucho, debes ser mayor de edad poder poder ingresar.`);
+        usuario = prompt(`¡Bienvenido a Fesno Negro ${usuario}! Ingrese su Nombre: `);
+        edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`);
+    }
+    let bienvenida = prompt(`¿Es tu primera vez en Fresno Negro? Ingrese Si o No`);
+    if (bienvenida.toUpperCase() === "SI"){
+        alert(`Bienvenido/a ${usuario}, es un placer tenerte con nosotros! Obtendras un descuento en tu primera compra`);
     }else {
-        alert(`Bienvenido/a nuevamente ${usuario}, nos alegra verte otra vez!`)
+        alert(`Bienvenido/a nuevamente ${usuario}, nos alegra verte otra vez!`);
     }
 }
 inicio()
-
-let cocina
-let gastototal
-let tipo
-let compra = (`Desea realizar una compra? Si No`)
-    if(compra === "Si")
-        tipo = prompt (`Elige una categoría: cocina baño`)
-    if(tipo==="cocina"){
-        cocina = prompt (`Elige un producto: BajoMesada AltoMesada`)
-            if(cocina === "BajoMesada")
-                precio = 40000
-            }else if (cocina==="AltoMesada"){
-                precio=45000
-    }
