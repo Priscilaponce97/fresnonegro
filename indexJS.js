03b0e8031c63989f1a049e4f871f9120f99c511e
Priscila Ponce <priscilabponce@gmail.com>
2022-12-13 12:08:59 -0300

    ...

--- indexJS.js
+++ indexJS.js
@@ -1,6 +1,22 @@
-function inicio (){
-    let usuario = prompt(`¡Bienvenido a Fesno Negro! Ingrese su Nombre`);
-    let edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`);
+const { value: formValues } = await Swal.fire({
+  title: 'Multiple inputs',
+  html:
+    '<input id="swal-input1" class="swal2-input">' +
+    '<input id="swal-input2" class="swal2-input">',
+  focusConfirm: false,
+  preConfirm: () => {
+    return [
+      document.getElementById('swal-input1').value,
+      document.getElementById('swal-input2').value
+    ]
+  }
+})
+
+if (formValues) {
+  Swal.fire(JSON.stringify(formValues))
+}
+  
+/*     let edad = prompt(`¿Cuál es tu edad ${usuario}? Ingresa un número:`);
     while (edad < 18){
         Swal.fire({
             title: 'Error!',
@@ -13,8 +29,6 @@ function inicio (){
     }
     let bienvenida = prompt(`¿Es tu primera vez en Fresno Negro? Ingrese Si o No`);
 
-    /* Se agrega mas manipulación del DOM: elimine 2 alert y los reemplace por un cambio en el h1. */
-
     if (bienvenida.toUpperCase() === "SI"){
         const bienvenidaUsuario = document.getElementById ("tituloBienvenida")
         bienvenidaUsuario.innerText=`Bienvenido/a ${usuario}, tenes un 40% off en tu primera compra!`
@@ -22,5 +36,4 @@ function inicio (){
         const bienvenidaUsuario = document.getElementById ("tituloBienvenida")
         bienvenidaUsuario.innerText=`Bienvenido/a ${usuario} a Fresno Negro`
     }
-}
-inicio()
+ */
